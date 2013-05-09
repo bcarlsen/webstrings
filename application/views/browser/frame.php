501194cad6829e808f8cdd2caf666f1ce4a2ae64
Brian Carlsen <carlsen.bri@gmail.com>
2013-05-09 15:14:50 -0600

    String Ribbon icons modified, drrop down menu's not working

--- application/views/browser/frame.php
+++ application/views/browser/frame.php
@@ -32,24 +32,7 @@
 					<div class="notes-scroller"></div>
 				</div>
 					
-				<div class="toolbar-item">
-					<div class="toolbar-icon">
-						<i class="icon icon-gear"></i>
-					</div>
-					<div class="toolbar-menu toolbar-settings">
-						<ul>
-							<li><a>Invite Friends</a></li>
-							<hr />
-							<li><a href="javascript:window.showModalDialog('<?php echo site_url('bookmarklet/index').'/'; ?>?url=' + location.href,'','dialogHeight:250px;dialogWidth:700px;center:yes;resizable:no;scroll:yes;')" onclick="alert('Drag to the bookmarks bar.'); return false;" style="cursor:move;">String It!</a></li>
-							<hr />
-							<li><a href="javascript:buildModal('<?php echo site_url('modals/settings'); ?>', 'modal-settings')">Settings</a></li>
-							<hr />
-							<li><a>About Us</a></li>
-							<hr />
-							<li><a href="<?php echo site_url('modals/log_out'); ?>/browser/index">Logout</a></li>
-						</ul>
-					</div>
-				</div>
+				
 			</div>
 			
 			<a class="view-all-strings-link" href="javascript:closeString()">&laquo; View All Strings</a>
--- application/views/browser/strings_ribbon.php
+++ application/views/browser/strings_ribbon.php
@@ -1,4 +1,24 @@
-<a class="ribbon-tool" href="javascript:buildModal('<?php echo site_url('modals/new_string'); ?>', 'modal-new-string')"><i class="icon icon-new-string"></i></a>
-<a class="ribbon-tool ribbon-tool-my-strings" href="javascript:refreshStrings('my_strings')"><i class="icon icon-my-strings string-filter"></i></a>
-<a class="ribbon-tool" href="javascript:refreshStrings('shared_strings')"><i class="icon icon-shared-strings string-filter"></i></a>
-
+<a class="ribbon-tool right-separator" href="javascript:buildModal('<?php echo site_url('modals/new_string'); ?>', 'modal-new-string')"><i class="icon icon-new-string"></i></a>
+<div class="icon right-separator"></div>
+<a class="ribbon-tool" href="<?php echo site_url('browser'); ?>"><i class="icon icon-home"></i></a>
+<a class="ribbon-tool"><i class="icon icon-globe"></i></a>
+<a class="ribbon-tool"><i class="icon icon-gear"></i></a>
+<div class="toolbar-item">
+	<a class="ribbon-tool">
+		<i class="icon icon-gear"></i>
+	</a>
+	<div class="toolbar-menu toolbar-settings">
+		<ul>
+			<li><a>Invite Friends</a></li>
+			<hr />
+			<li><a href="javascript:window.showModalDialog('<?php echo site_url('bookmarklet/index').'/'; ?>?url=' + location.href,'','dialogHeight:250px;dialogWidth:700px;center:yes;resizable:no;scroll:yes;')" onclick="alert('Drag to the bookmarks bar.'); return false;" style="cursor:move;">String It!</a></li>
+			<hr />
+			<li><a href="javascript:buildModal('<?php echo site_url('modals/settings'); ?>', 'modal-settings')">Settings</a></li>
+			<hr />
+			<li><a>About Us</a></li>
+			<hr />
+			<li><a href="<?php echo site_url('modals/log_out'); ?>/browser/index">Logout</a></li>
+		</ul>
+	</div>
+</div>
+				
