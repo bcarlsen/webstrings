6117e8033a5c084ce81907863be2151d2c2f0cc4
Brian Carlsen <carlsen.bri@gmail.com>
2013-05-09 10:53:20 -0600

    New landing page

--- application/views/browser/not_logged_in/frame.php
+++ application/views/browser/not_logged_in/frame.php
@@ -1,16 +1,57 @@
+<div class="alternative_registration_page">
 
-<div id="browser">
-	<a id="hideMe"><span>Hide Bar</span></a>
-	<div class="header">
-		<a href="<?php echo site_url('browser'); ?>"><h1>WEBSTRINGS</h1></a>
-	</div>
-	
-	<div class="ribbon">
-		<div class="ribbon-text-wrapper">
-			<a class="ribbon-text-large" target="iframe" href="<?php echo site_url('internalPages/new_user_registration'); ?>">SIGN UP</a>
+	<div class="intro_video_box sign_up_box">
+
+		<h1 class="what_is_text sign_up_title">Sign Up For Webstrings</h1>
+		
+		<div class="form_errors">
+			<?php echo validation_errors(); ?>
 		</div>
-	</div>
-	
+		<div class="sign_up_fields">
+			<form action="<?php echo site_url('internalPages/new_user_registration'); ?>" method="post">
+			<?php //echo form_open('internalPages/sign_up_view'); ?>
+			<div class="control-group">
+					<label for="f_name">First Name</label>
+					<span class="input-wrapper">
+						<input type="text" name="fname" value="<?php echo set_value('fname'); ?>" />
+					</span>
+					
+					<label for="l_name">Last Name</label>
+					<span class="input-wrapper">
+						<input type="text" name="lname" value="<?php echo set_value('lname'); ?>" />
+					</span>
+				</div>
+				<div class="control-group">
+					<label for="email">Email</label>
+					<span class="input-wrapper">
+						<input type="text" name="email" value="<?php echo set_value('email'); ?>" />
+					</span>
+				</div>
+				<div class="control-group">
+					<label for="password">Password</label>
+					<span class="input-wrapper">
+						<input type="password" name="password" />
+					</span>
+				</div>
+				<div class="control-group">
+					<label for="password_conf">Confirm Password</label>
+					<span class="input-wrapper">
+						<input type="password" name="password_conf" />
+					</span>
+				</div>
+				<?php //echo validation_errors('<div class="control-group"><p>','</p></div>'); ?>
+				<div class="control-group">
+					<input type="submit" name="submit" value="Sign Up" />
+				</div>
+			</form>
+		</div>
+
+
+		<!--<h2 >(This Is A Video)</h2>-->
+			
+
+			
+<div id="browser">
 	<form action="<?php echo site_url('browser/log_in'); ?>" method="post">
 		<div class="control-group">
 			<label for="email">Email</label>
@@ -33,11 +74,8 @@
 	</div>
 	
 </div>
+		
 
 
-
-<div id="content">
-	<div class="nav-arrow nav-arrow-left"><a href="javascript:prevPage()"><i class="icon icon-arrow-left"></i></a></div>
-	<iframe name="iframe" src="<?php echo site_url('internalPages/getting_started'); ?>">Your browser does not support iframes.</iframe>
-	<div class="nav-arrow nav-arrow-right"><a href="javascript:nextPage()"><i class="icon icon-arrow-right"></i></a></div>
+	</div>
 </div>
