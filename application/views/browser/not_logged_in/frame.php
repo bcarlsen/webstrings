6070a391734fd14b450d07f9ff1df28b6e9d6b45
Daniel Haarburger <work.haarburger@gmail.com>
2013-05-09 15:10:26 -0600

    finished log in page

--- application/views/browser/not_logged_in/frame.php
+++ application/views/browser/not_logged_in/frame.php
@@ -1,81 +1,113 @@
-<div class="alternative_registration_page">
+<div class="sign_up_page">
+	<div class="top_nav">
+		<ul>
+			<li><a href="www.google.com">Press</a></li>
+			<li><a href="www.google.com">Contact</a></li>
+			<li><a href="www.google.com">About Us</a></li>
+		</ul>
 
-	<div class="intro_video_box sign_up_box">
-
-		<h1 class="what_is_text sign_up_title">Sign Up For Webstrings</h1>
-		
-		<div class="form_errors">
-			<?php echo validation_errors(); ?>
+	</div>	
+	<div class="logo_header_bar">
+		<div class="central_text_logo"></div>
+	</div>
+	<div class="blue_body_bar">
+		<div class="central_tag_image"></div>
+		<div class="welcome_text">
+			<h1>Simple, Collaborative Research"</h1>
+			<h2>We're still testing. Will be live soon</h2>
 		</div>
-		<div class="sign_up_fields">
+
+		<div class="home_page_block">
 			<form action="<?php echo site_url('internalPages/new_user_registration'); ?>" method="post">
-			<?php //echo form_open('internalPages/sign_up_view'); ?>
-			<div class="control-group">
-					<label for="f_name">First Name</label>
-					<span class="input-wrapper">
-						<input type="text" name="fname" value="<?php echo set_value('fname'); ?>" />
-					</span>
-					
-					<label for="l_name">Last Name</label>
-					<span class="input-wrapper">
-						<input type="text" name="lname" value="<?php echo set_value('lname'); ?>" />
-					</span>
+				<div class="float_left under_half_width">
+				<?php //echo form_open('internalPages/sign_up_view'); ?>		
+
+					<div class="control-group">
+						<ul>
+							<!-- First Name Label -->
+							<!-- <label for="f_name">First Name</label> -->
+							<li>
+								<span class="input-wrapper">
+									<input type="text" name="fname" value="<?php echo set_value('fname'); ?>" placeholder="   First Name"/>
+								</span>
+							</li>
+							
+							<!-- Last Name Label -->
+							<!--<label for="l_name">Last Name</label>-->
+							<li>
+								<span class="input-wrapper">
+									<input type="text" name="lname" value="<?php echo set_value('lname'); ?>" placeholder="   Last Name"/>
+								</span>
+							</li>
+						</ul>
+					</div>
+
 				</div>
-				<div class="control-group">
-					<label for="email">Email</label>
-					<span class="input-wrapper">
-						<input type="text" name="email" value="<?php echo set_value('email'); ?>" />
-					</span>
+
+				<div class="float_right under_half_width">
+					<!-- Email Label Name Label -->
+					<div class="control-group">
+						<span class="input-wrapper">
+							<input type="text" name="email" value="<?php echo set_value('email'); ?>" placeholder="   Your Email Address" />
+						</span>
+					</div>
+
+					<!-- Password Label -->
+					<div class="pass_and_sign_up">
+						<div class="control-group float_left under_half_width">
+							<span class="input-wrapper">
+								<input type="password" name="password" placeholder="   Secret Password"/>
+							</span>
+						</div>
+						<div class="control-group float_right under_half_width">
+							<input type="submit" name="submit" value="Sign Up" />
+						</div>
+					</div>
 				</div>
-				<div class="control-group">
-					<label for="password">Password</label>
-					<span class="input-wrapper">
-						<input type="password" name="password" />
-					</span>
-				</div>
+				<!--
+				Confirm Password Lable
 				<div class="control-group">
 					<label for="password_conf">Confirm Password</label>
 					<span class="input-wrapper">
 						<input type="password" name="password_conf" />
 					</span>
-				</div>
+				</div> 
+				-->
 				<?php //echo validation_errors('<div class="control-group"><p>','</p></div>'); ?>
-				<div class="control-group">
-					<input type="submit" name="submit" value="Sign Up" />
-				</div>
 			</form>
 		</div>
 
-
-		<!--<h2 >(This Is A Video)</h2>-->
+		<hr>
 			
+		<div class="home_page_block ">
+			<form action="<?php echo site_url('browser/log_in'); ?>" method="post">
+				<div class="control-group under_half_width float_left">
+					<span class="input-wrapper">
+						<input type="text" name="email" value="<?php echo set_value('email'); ?>" placeholder="   Your Email Address"/>
+					</span>
+				</div>
+				<div class="under_half_width float_right">
+					<div class="control-group under_half_width float_left">
+						<span class="input-wrapper">
+							<input type="password" name="password" placeholder="   Your Password"/>
+						</span>
+					</div>
+					<div class="control-group under_half_width float_right">
+						<input type="submit" name="submit" value="Login" />
+					</div>
+				</div>
+			</form>
+			</div>
+			<div class="form_errors">
+					<?php echo validation_errors(); ?>
+			</div>
+		</div>
 
-			
-<div id="browser">
-	<form action="<?php echo site_url('browser/log_in'); ?>" method="post">
-		<div class="control-group">
-			<label for="email">Email</label>
-			<span class="input-wrapper">
-				<input type="text" name="email" value="<?php echo set_value('email'); ?>"/>
-			</span>
-		</div>
-		<div class="control-group">
-			<label for="password">Password</label>
-			<span class="input-wrapper">
-				<input type="password" name="password" />
-			</span>
-		</div>
-		<div class="control-group">
-			<input type="submit" name="submit" value="Login" />
-		</div>
-	</form>
-	<div class="form_errors">
-			<?php echo validation_errors(); ?>
 	</div>
+
+
 	
+
 </div>
-		
 
 
-	</div>
-</div>
